7b5745d3f0863d636d94c9c6a389486ee8ffbbff
biplabmahanty <bmahanty501@gmail.com>
2024-07-30 11:47:21 +0530

    complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringUpdated = '';
+    if (num < 0){
+        return "ERROR";
+    }
+    for(let i=0; i<num ; i++){
+        stringUpdated = stringUpdated.concat(string) ;
+    }
+
+    return stringUpdated;
 
 };
 
