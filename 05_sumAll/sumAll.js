1700e36a305195360c3e4f05d82a24528abc4541
biplabmahanty <bmahanty501@gmail.com>
2024-07-30 15:32:46 +0530

    complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 1 || num2 < 1 ) return "ERROR";
+  if (num1 % 1 !== 0 || num2 % 1 !== 0) return "ERROR";
+  if((typeof num1 !== "number") || (typeof num2 !== "number")) return "ERROR";
 
+  let largerNumber = Math.max(num1,num2);
+  let smallerNumber = Math.min(num1,num2);
+  let totalSum = 0;
+  for (let i = smallerNumber; i <= largerNumber; i++) {
+    totalSum += i;
+  }
+  return totalSum;
 };
 
 // Do not edit below this line
