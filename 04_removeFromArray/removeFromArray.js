f64119c16aae8c86eb690ce3b2d02858a28fed76
biplabmahanty <bmahanty501@gmail.com>
2024-07-30 14:44:41 +0530

    complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+  const itemsArray = [...items];
+  for (const item of itemsArray) {
+    while (true) {
+      const index = array.indexOf(item);
+      if(index > -1) {
+        array.splice(index, 1);
+      }else{
+        break;
+      }
+    }
+
+    // if (index > -1){
+    //     array.splice(index,1)
+    // }
+  }
+  return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//
