9c2ab486c5f3b2b56e577214195da5fc7fc3d43e
biplabmahanty <bmahanty501@gmail.com>
2024-07-31 10:05:08 +0530

    complete 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0){
+        if (year % 100 === 0 && !(year % 400 === 0)){
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
+
+// if div by 4:
+//  if div by 100 but not by 400 : not leap year
+//  else : leap year
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
